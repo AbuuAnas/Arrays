8c69f6ec02a2d4847d729a8760cddc8d8b951341
ICT <ICT@DESKTOP-U5PI05R>
2022-10-16 11:00:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Arrays
+{
+
+    class Program
+    {
+        //we add a square bracket []
+        //after parameter's data type in the method declaration
+
+        public static void PrintFirstElement(int[] a)
+        {
+            Console.WriteLine("The First Element is {0}.\n", a[0]);
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5 };
+            PrintFirstElement(myArray);
+        }
+    }
+}
